dd43fd6db0cafc54f8c4afcbbfa04735a9bec1ed
thomastepi <tepexminem2013@gmail.com>
2024-01-15 16:55:16 -0500

    db file path

---
+++ mocks/__init__.py
@@ -0,0 +1,2 @@
+from .user_data import USERS
+DB_FILE_PATH = 'entities//twitter.db'
---
+++ mocks/user_data.py
@@ -0,0 +1,12 @@
+
+USERS = """[
+{
+    'name': 'John Wayne',
+    'email': 'jwayne@gmail.net'
+},
+{
+    'name': 'Jane Doe',
+    'email': 'jdoe@gmail.net'
+}
+]
+"""
